c66b797c3ea5820ee1975b719be0b9690db9785f
lizhihao97 <940166841@qq.com>
2018-12-24 09:34:04 +0800

    init

---
+++ src/main/java/com/example/flowerpurchase/FlowerPurchaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.flowerpurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerPurchaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerPurchaseApplication.class, args);
+    }
+
+}
+
